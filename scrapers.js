1362bb77f41573a286135a3275e8a897d0583876
Francisco Javier Plaza Urrutia <fjplaurr@gmail.com>
2020-09-13 18:51:52 +0200

    add express and get route for home

--- scrapers.js
+++ scrapers.js
@@ -1,7 +1,10 @@
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+const express = require('express');
 
 let browser;
+
+const app = express();
 
 // Plugin to avoid bot searchers
 puppeteer.use(StealthPlugin());
@@ -22,10 +25,14 @@ async function launchBrowser() {
 }
 
 // Main function
-(async function main() {
+async function main() {
   await launchBrowser();
   scrapeFlat('https://www.idealista.com/alquiler-viviendas/almeria-almeria/con-precio-hasta_500,de-dos-dormitorios,de-tres-dormitorios,de-cuatro-cinco-habitaciones-o-mas,publicado_ultimas-24-horas');
   scrapeFlat('https://www.fotocasa.es/es/alquiler/viviendas/almeria-capital/almeria-ciudad/l?gridType=list&combinedLocationIds=724,1,4,276,486,4013,88,0,0&latitude=36.838519415269104&longitude=-2.4599311887141835&maxPrice=500&minRooms=2&sortType=publicationDate');
   scrapeFlat('https://www.milanuncios.com/alquiler-de-pisos-en-almeria-almeria/?fromSearch=1&desde=350&hasta=500&demanda=n&dormd=2');
   scrapeFlat('https://www.pisos.com/alquiler/pisos-almeria_capital/con-2-habitaciones/desde-350/hasta-500/fecharecientedesde-desc');
-}());
+}
+
+app.get('/', (req, res) => {
+  main();
+});
